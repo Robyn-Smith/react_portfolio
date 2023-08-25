64ab8104fa9eb42fc6f25f9a81dd512d58e34845
Robyn Smith <robynsmith21@hotmail.com>
2023-08-25 18:33:15 +0100

    changes to app.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import { useState } from "react";
+// import logo from './logo.svg';
 import './App.css';
+import Layout from "./components/Layout";
+
+import PortfolioDashboard from "./components/PortfolioDashboard";
+import Contact from "./components/Contact";
+import About from "./components/About";
+
+const pages = ["portfolio", "about", "contact"];
 
 function App() {
+  const [page, setPage] = useState("portfolio");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Layout pages={pages} onSetPage={setPage}>
+      {page === "portfolio" && <PortfolioDashboard />}
+      {page === "about" && <About />}
+      {page === "contact" && <Contact />}
+    </Layout>
+  )
 }
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
