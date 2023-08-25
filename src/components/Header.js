5ab54fd5cdc057c38c607cae2aaea46edf2cbc2c
Robyn Smith <robynsmith21@hotmail.com>
2023-08-25 18:44:36 +0100

    added to header

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Nav from "./Nav";
+
+function Header({ pages, onSetPage }) {
+    return (
+        <Header className="masthead mb-auto">
+            <div className="inner">
+                <h3 className="masthead-brand">Portfolio</h3>
+                <Nav pages={pages} onSetPage={onSetPage}/>
+            </div>
+        </Header>
+    );
+}
+
+export default Header;
