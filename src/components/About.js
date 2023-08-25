5ad7d85bbcdf3cf9aeb3bb9c4a28d31461ad933e
Robyn Smith <robynsmith21@hotmail.com>
2023-08-25 18:56:04 +0100

    added to about.js

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+function About() {
+    return (
+        <Row>
+            <Col sm={12}>
+                <h1>About Me</h1>
+            </Col>
+        </Row>
+    );
+}
+
+export default About;
