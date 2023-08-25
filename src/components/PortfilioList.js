4b93c5d2f9560eb4d5dc2b8acbb25a18ff3d1796
Robyn Smith <robynsmith21@hotmail.com>
2023-08-25 19:09:16 +0100

    changes to portfiliolist

---
+++ src/components/PortfilioList.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import PortfilioItem from './PortfilioItem';
+
+function PortfilioList({ portfolio, onFilter }) {
+    return (
+        <div className="w-100">
+            <div className="portfolio-filter">
+                <select onChange={onFilter}>
+                    <option value="">All</option>
+                    <option value="react">React</option>
+                    <option value="heroku">Heroku</option>
+                    <option value="nextjs">NextJS</option>
+                    <option value="firebase">Firebase</option>
+                </select>
+            </div>
+            {
+                portfolio.map((item, index) => {
+                    return (<PortfilioItem item={item} />)
+                })
+            }
+        </div>
+    )
+}
+
+export default PortfilioList
