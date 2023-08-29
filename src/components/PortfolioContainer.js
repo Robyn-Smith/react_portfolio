d0c59e19c7e9ceaac7a26da699c6a81be6a14736
Robyn Smith <robynsmith21@hotmail.com>
2023-08-29 21:01:58 +0100

    issues with bootstrap new starter code

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import Portfolio from './pages/Portfolio';
+import About from './pages/About';
+import Resume from './pages/Resume';
+import Contact from './pages/Contact';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('Portfolio');
+
+  const renderPage = () => {
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </div>
+  );
+}
---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Me Page</h1>
+      <p>
+        Nunc aliquet risus ac finibus porta. Nam quis arcu non
+        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
+        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
+        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+      </p>
+    </div>
+  );
+}
