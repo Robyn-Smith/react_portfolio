0f28c96a0b7185eaebd675d6911c5d7965326f69
Robyn Smith <robynsmith21@hotmail.com>
2023-08-29 23:06:37 +0100

    added about me writing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import { useState } from "react";
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import logo from './logo.svg';
+// import { useState } from "react";
+// // import 'bootstrap/dist/css/bootstrap.min.css';
+// // import logo from './logo.svg';
+// import './App.css';
+// import Layout from "./components/Layout";
+
+// import PortfolioDashboard from "./components/PortfolioDashboard";
+// import Contact from "./components/Contact";
+// import About from "./components/About";
+
+// const pages = ["portfolio", "about", "contact"];
+
+// function App() {
+//   const [page, setPage] = useState("portfolio");
+
+//   return (
+//     <Layout pages={pages} onSetPage={setPage}>
+//       {page === "portfolio" && <PortfolioDashboard />}
+//       {page === "about" && <About />}
+//       {page === "contact" && <Contact />}
+//     </Layout>
+//   )
+// }
+// // function App() {
+// //   return (
+// //     <div className="App">
+// //       <header className="App-header">
+// //         <img src={logo} className="App-logo" alt="logo" />
+// //         <p>
+// //           Edit <code>src/App.js</code> and save to reload.
+// //         </p>
+// //         <a
+// //           className="App-link"
+// //           href="https://reactjs.org"
+// //           target="_blank"
+// //           rel="noopener noreferrer"
+// //         >
+// //           Learn React
+// //         </a>
+// //       </header>
+// //     </div>
+// //   );
+// // }
+
+// export default App;
+
+
+import logo from './logo.svg';
 import './App.css';
-import Layout from "./components/Layout";
-
-import PortfolioDashboard from "./components/PortfolioDashboard";
-import Contact from "./components/Contact";
-import About from "./components/About";
-
-const pages = ["portfolio", "about", "contact"];
-
-function App() {
-  const [page, setPage] = useState("portfolio");
-
-  return (
-    <Layout pages={pages} onSetPage={setPage}>
-      {page === "portfolio" && <PortfolioDashboard />}
-      {page === "about" && <About />}
-      {page === "contact" && <Contact />}
-    </Layout>
-  )
-}
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
 // function App() {
 //   return (
 //     <div className="App">
@@ -41,5 +69,6 @@ function App() {
 //     </div>
 //   );
 // }
+const App = () => <PortfolioContainer />;
 
 export default App;
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,12 +3,36 @@ import React from 'react';
 export default function About() {
   return (
     <div>
-      <h1>About Me Page</h1>
+      <h1>About Me</h1>
       <p>
-        Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+      A career in coding is the right choice for me because I enjoy problem solving, creativity 
+      and critical thinking. Having recently completed my degree in Integrated Working with Children 
+      and Families, including a number of placements and paid employment, I decided to change career 
+      path. As a result, I have researched alternative careers and courses, decided to learn Python 
+      and quickly gained an interest in coding. I am a self-motivated, high achiever and work well in 
+      both team and individual settings. With additional coding skills I believe that I can apply 
+      myself to a career in the digital sector.
+
+      I have a strong academic record, my highest achievement being a 1st class BA honours degree. 
+      I also have 10 GCSEs at grade C or above (including 3 Bs, 3 As and an A*) and two Cache 
+      diplomas, both at grade A.  In learning and working environments I am attentive and 
+      enthusiastic. I am keen to excel at coding and utilise my other strengths to bring more 
+      than just technical competence into a future role.  Dedication and commitment to learning are 
+      key to my success.  I complete all coursework on time and to a high standard and achieve 
+      excellent results even in challenging circumstances; the best example being a 1st class 
+      honours degree during the Covid-19 pandemic!
+
+      Many of the skills I have learned to date will be beneficial to my future role in the 
+      technology sector. These include excellent organisational, leadership and social skills as 
+      well as strong mathematical, problem-solving skills. I am familiar and competent with the 
+      latest technology; often helping others with technological issues. I am also keen to tap 
+      into my creative side, having a flair for art.  
+
+      I am keen to apply my diverse skillset into coding and pursue a career in the tech industry. 
+      I am a committed, conscientious and a determined learner. I complete work to a very high 
+      standard and achieve excellent results.  I also receive excellent feedback from teachers, 
+      tutors and lecturers as well as employers.  Given these attributes, I know that I can achieve 
+      an excellent outcome from the bootcamp and embark on a successful career in technology.
       </p>
     </div>
   );
