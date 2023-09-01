15d5e8ec21694873b83d442695b34f8a816fb403
Robyn Smith <robynsmith21@hotmail.com>
2023-09-01 05:25:54 +0100

    commented out code for future dev

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import styles from "../index.css"
+// import styles from "../index.css"
+// className={styles.responsive}
 
 function NavTabs({ currentPage, handlePageChange }) {
   return (
