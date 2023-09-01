b8993f16a519e0bd3ced9cb3c7df09512904f14e
Robyn Smith <robynsmith21@hotmail.com>
2023-09-01 03:34:05 +0100

    changed order of portfolio

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -11,6 +11,15 @@ export default function Portfolio() {
         During my bootcamp full stack development course I completed multiple projects. This supported my knowledge, gave me experience and supported my confidence in coding. Please click the images below to view my projects.
       </p>
       <section class="box">
+            <h3 style={{ color: '#b62075' }}>Group Project 1: Picturesque</h3>
+            <a href="https://robyn-smith.github.io/The-Dev-Society/">
+              <img class= "picturesque" alt="picturesque" src={picturesque}></img>
+            </a>
+            <p>
+            This was a group project was a movie website, allowing users to create personal watchlists, watch trailers and search for and learn more about films including actors that star in them, plot, place of creation, genre, imbd rating and date of creation. This aplication was created using HTML 5, CSS and JavaScript and used 4 API's including GeoCode -for City/Country to Latitude/Longitude, OMDB- for Movie details, YouTube- for Movie Trailer from YouTube and Google Map- for showing Country in Google Map.
+            </p>
+          </section>
+      <section class="box">
             <h3 style={{ color: '#b62075' }}>Group Project 2: Music Portal</h3>
             <a href="https://front-end-society-2d48b234d58b.herokuapp.com">
               <img class= "project2" alt="Music portal" src={project2}></img>
@@ -20,12 +29,12 @@ export default function Portfolio() {
             </p>
           </section>
           <section class="box">
-            <h3 style={{ color: '#b62075' }}>Group Project 1: Picturesque</h3>
-            <a href="https://robyn-smith.github.io/The-Dev-Society/">
-              <img class= "picturesque" alt="picturesque" src={picturesque}></img>
+            <h3 style={{ color: '#b62075' }}>My first project: Code Refactor </h3>
+            <a href="https://robyn-smith.github.io/week-1-code-refactor-challenge/">
+              <img class= "first-project-img" alt="Horiseon Webpage" src={week1}></img>
             </a>
             <p>
-            This was a group project was a movie website, allowing users to create personal watchlists, watch trailers and search for and learn more about films including actors that star in them, plot, place of creation, genre, imbd rating and date of creation. This aplication was created using HTML 5, CSS and JavaScript and used 4 API's including GeoCode -for City/Country to Latitude/Longitude, OMDB- for Movie details, YouTube- for Movie Trailer from YouTube and Google Map- for showing Country in Google Map.
+              This project is a code refactor to support a marketing agency website. The website contains information about Search Engine Optimisation, Online Reputation Management, Social Media Marketing, Lead Generation, Brand Awareness and Cost Management. It contains header, navigation, aside, section, figure, article and footer elements; which are styled in CSS. Within this code refactor I updated the title, fixed a link, replaced repetitive div tags with semantic elements, added alternative text descriptions to images and updated the CSS acordingly. As a result, this made the website more modern and suitible for HTML 5, more accessible for disabled users, improved funtionality and supported search engine optimisation.
             </p>
           </section>
           <section class="box">
