a8faec79c3be80d19f237d0edaf81a99a656db95
Robyn Smith <robynsmith21@hotmail.com>
2023-09-01 03:53:31 +0100

    added quiz to portfolio

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -12,6 +12,15 @@ export default function Portfolio() {
         During my bootcamp full stack development course I completed multiple projects. This supported my knowledge, gave me experience and supported my confidence in coding. Please click the images below to view my projects.
       </p>
       <section class="box">
+            <h3 style={{ color: '#b62075' }}>Web API: Quiz</h3>
+            <a href="https://robyn-smith.github.io/week-4-Web-API-Quiz/">
+              <img class= "projectpic" alt="web API quiz" src={quiz}></img>
+            </a>
+            <p>
+              This project used HTML, CSS and JavaScript to create a web API quiz. The quiz supports the users knowlege of coding by challenging the user to answer the multiple choice options correctly. The quiz uses audio files and messages to tell the user if they have answers right or wrong. The timer counts down at the top of the page limiting the users time to answer the question, when the user answers wrong the timer decrements even quicker. Finally, the quiz uses local storage to remmeber usernames and their highscores in the quiz.
+            </p>
+          </section>
+      <section class="box">
             <h3 style={{ color: '#b62075' }}>Group Project 1: Picturesque</h3>
             <a href="https://robyn-smith.github.io/The-Dev-Society/">
               <img class= "projectpic" alt="picturesque" src={picturesque}></img>
@@ -38,15 +47,7 @@ export default function Portfolio() {
               This project is a code refactor to support a marketing agency website. The website contains information about Search Engine Optimisation, Online Reputation Management, Social Media Marketing, Lead Generation, Brand Awareness and Cost Management. It contains header, navigation, aside, section, figure, article and footer elements; which are styled in CSS. Within this code refactor I updated the title, fixed a link, replaced repetitive div tags with semantic elements, added alternative text descriptions to images and updated the CSS acordingly. As a result, this made the website more modern and suitible for HTML 5, more accessible for disabled users, improved funtionality and supported search engine optimisation.
             </p>
           </section>
-          <section class="box">
-            <h3 style={{ color: '#b62075' }}>Web API: Quiz</h3>
-            <a href="https://robyn-smith.github.io/week-4-Web-API-Quiz/">
-              <img class= "projectpic" alt="web API quiz" src={quiz}></img>
-            </a>
-            <p>
-              This project is a code refactor to support a marketing agency website. The website contains information about Search Engine Optimisation, Online Reputation Management, Social Media Marketing, Lead Generation, Brand Awareness and Cost Management. It contains header, navigation, aside, section, figure, article and footer elements; which are styled in CSS. Within this code refactor I updated the title, fixed a link, replaced repetitive div tags with semantic elements, added alternative text descriptions to images and updated the CSS acordingly. As a result, this made the website more modern and suitible for HTML 5, more accessible for disabled users, improved funtionality and supported search engine optimisation.
-            </p>
-          </section>
+          
 
     </div>
   );
