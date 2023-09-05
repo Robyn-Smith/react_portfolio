388a6beae6454a62f4594f9b07dd6aa01ec18630
Robyn Smith <robynsmith21@hotmail.com>
2023-09-05 03:22:37 +0100

    changed to hover effect footer issues

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,36 +17,36 @@ function Footer() {
                     src={instagram}
                     alt="instagram"
                     style={{width: 40, paddingRight: '5%'}}
-                    onMouseOver={e => e.currentTarget.style.opacity = 1}
-                    onMouseOut={e => e.currentTarget.style.opacity = 0.3}
+                    onMouseOver={e => e.currentTarget.style.opacity = 0.3}
+                    onMouseOut={e => e.currentTarget.style.opacity = 1}
                 /></a> 
             <a href="https://twitter.com"><img
                     src={twitter}
                     alt="twitter"
                     style={{width: 40, paddingLeft: '5%', paddingRight: '5%'}}
-                    onMouseOver={e => e.currentTarget.style.opacity = 1}
-                    onMouseOut={e => e.currentTarget.style.opacity = 0.3}
+                    onMouseOver={e => e.currentTarget.style.opacity = 0.3}
+                    onMouseOut={e => e.currentTarget.style.opacity = 1}
                 /></a>
                 <a href="https://github.com/Robyn-Smith?tab=repositories"><img
                     src={github}
                     alt="github"
                     style={{width: 40, paddingLeft: '5%', paddingRight: '5%'}}
-                    onMouseOver={e => e.currentTarget.style.opacity = 1}
-                    onMouseOut={e => e.currentTarget.style.opacity = 0.3}
+                    onMouseOver={e => e.currentTarget.style.opacity = 0.3}
+                    onMouseOut={e => e.currentTarget.style.opacity = 1}
                 /></a>
                 <a href="https://www.linkedin.com"><img
                     src={linkedin}
                     alt="linkedin"
                     style={{width: 40, paddingLeft: '5%', paddingRight: '5%'}}
-                    onMouseOver={e => e.currentTarget.style.opacity = 1}
-                    onMouseOut={e => e.currentTarget.style.opacity = 0.2}
+                    onMouseOver={e => e.currentTarget.style.opacity = 0.2}
+                    onMouseOut={e => e.currentTarget.style.opacity = 1}
                 /></a>
                 <a href="https://www.facebook.com"><img
                     src={facebook}
                     alt="facebook"
                     style={{width: 40, paddingLeft: '5%'}}
-                    onMouseOver={e => e.currentTarget.style.opacity = 1}
-                    onMouseOut={e => e.currentTarget.style.opacity = 0.3}
+                    onMouseOver={e => e.currentTarget.style.opacity = 0.3}
+                    onMouseOut={e => e.currentTarget.style.opacity = 1}
                 /></a>
             </div>
             <span style={{ color: 'white',}}>© Robyn Smith 2023</span>
