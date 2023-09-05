d9fcac0ef43ff457d4ba288e6906e3a8948a91d4
Robyn Smith <robynsmith21@hotmail.com>
2023-09-05 02:40:56 +0100

    added twitter and instagram to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,71 +1,50 @@
-// import React from "react";
-
-// function Footer() {
-//     return <div>Copyright JSON 2023 &copy;</div>;
-// }
-
-
-// <footer id="Contact" class="Contact">
-//         <h4>
-//           Contact Information
-//         </h4>
-//         <ul>
-//           <li>📩 Email: robynsmith@hotmail.co.uk</li>
-//           <li>📱 Mobile: 07777777774</li>
-//           <li>☎️ Landline: 01334 123454</li>
-//         </ul>
-//       </footer>
-
-// export default Footer;
-//import '../styles/Header.css';
-
 import React from 'react';
 import github from '../assets/github-icon-logo-png-transparent.png';
 import linkedin from '../assets/linkedin-icon-logo-png-transparent.png';
 import facebook from '../assets/facebook-3-logo-png-transparent.png';
-
-const styles = {
-    display: 'flex',
-    flexDirection: "column",
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: '2%',
-    background: '#b6207562',
-};
-
-const socialStyle = {
-    display: 'flex',
-    flexDirection: "row",
-    justifyContent: 'center',
-    padding: "1%",
-}
-
-const imgStyle = {
-    width: 32,
-}
+import twitter from '../assets/twitter.png';
+import instagram from '../assets/instagram.png';
 
 function Footer() {
     return (
-        <footer style={styles}>
-            <div style={socialStyle}>
+        <footer style={{display: 'flex',
+        flexDirection: "column",
+        justifyContent: 'center',
+        alignItems: 'center',
+        background: '#b6207562',}}>
+            <div style={{display: 'flex', flexDirection: "row", justifyContent: 'center', padding: "1%",}}>
+            <a href="https://instagram.com"><img
+                    src={instagram}
+                    alt="instagram"
+                    style={{width: 40, paddingRight: '5%'}}
+                    onMouseOver={e => e.currentTarget.style.opacity = 1}
+                    onMouseOut={e => e.currentTarget.style.opacity = 0.3}
+                /></a> 
+            <a href="https://twitter.com"><img
+                    src={twitter}
+                    alt="twitter"
+                    style={{width: 40, paddingLeft: '5%', paddingRight: '5%'}}
+                    onMouseOver={e => e.currentTarget.style.opacity = 1}
+                    onMouseOut={e => e.currentTarget.style.opacity = 0.3}
+                /></a>
                 <a href="https://github.com/Robyn-Smith?tab=repositories"><img
                     src={github}
                     alt="github"
-                    style={imgStyle}
+                    style={{width: 40, paddingLeft: '5%', paddingRight: '5%'}}
                     onMouseOver={e => e.currentTarget.style.opacity = 1}
                     onMouseOut={e => e.currentTarget.style.opacity = 0.3}
                 /></a>
                 <a href="https://www.linkedin.com"><img
                     src={linkedin}
                     alt="linkedin"
-                    style={imgStyle}
+                    style={{width: 40, paddingLeft: '5%', paddingRight: '5%'}}
                     onMouseOver={e => e.currentTarget.style.opacity = 1}
                     onMouseOut={e => e.currentTarget.style.opacity = 0.2}
                 /></a>
                 <a href="https://www.facebook.com"><img
                     src={facebook}
                     alt="facebook"
-                    style={imgStyle}
+                    style={{width: 40, paddingLeft: '5%'}}
                     onMouseOver={e => e.currentTarget.style.opacity = 1}
                     onMouseOut={e => e.currentTarget.style.opacity = 0.3}
                 /></a>
